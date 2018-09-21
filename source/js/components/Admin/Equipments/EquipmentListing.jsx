78c86cf22add1866c3e80e4f1d1cc5d7d123540b
sbnarola <sb@narola.email>
2018-09-21 10:00:39 +0530

    Changes in equipments listing

--- source/js/components/Admin/Equipments/EquipmentListing.jsx
+++ source/js/components/Admin/Equipments/EquipmentListing.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { adminRouteCodes } from '../../../constants/adminRoutes';
 import SweetAlert from "react-bootstrap-sweetalert";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ReactTable from "react-table";
 import { SERVER_BASE_URL } from '../../../constants/consts';
 import noImg from 'img/common/no-img.png';
@@ -53,22 +53,15 @@ class EquipmentListing extends Component {
         const { equipmentCategories } = this.props;
         return (
             <div className="equipment-listing-wrapper">
-                <div className="body-head space-btm-45 d-flex justify-content-start">
-                    <div className="body-head-l">
-                        <h2>Equipments</h2>
-                    </div>
-                    <div className="body-head-r">
-                        <NavLink to={adminRouteCodes.EQUIPMENTS_SAVE} className="pink-btn">
-                            <i className="icon-add_circle"></i> Add Equipment
-                        </NavLink>
-                    </div>
-                </div>
-
-                <div className="body-content row d-flex">
+                <div className="body-content row d-flex my-panel-body">
                     <div className="col-md-12">
                         <div className="white-box">
                             <div className="whitebox-head">
                                 <h3 className="title-h3">Equipments List</h3>
+                                <Link to={adminRouteCodes.EQUIPMENTS_SAVE} className="add-new-btn">
+                                    <span>Add Equipment</span>
+                                    <i className="icon-add_circle"></i>
+                                </Link>
                             </div>
                             <div className="row d-flex whitebox-body">
                                 <div className="col-md-12">
@@ -219,37 +212,26 @@ class EquipmentListing extends Component {
                                                 accessor: "_id",
                                                 filterable: false,
                                                 sortable: false,
-                                                maxWidth: 100,
+                                                maxWidth: 70,
                                                 Cell: (row) => {
                                                     return (
                                                         <div className="actions-wrapper">
-                                                            <ButtonToolbar>
-                                                                <DropdownButton title="Actions" pullRight id="dropdown-size-medium">
-                                                                    <MenuItem
-                                                                        eventKey="1"
-                                                                        href={`${adminRouteCodes.EQUIPMENTS_SAVE}/${row.value}`}
-                                                                        onClick={(e) => {
-                                                                            e.preventDefault();
-                                                                            this.props.history.push(`${adminRouteCodes.EQUIPMENTS_SAVE}/${row.value}`);
-                                                                        }}
-                                                                    >
-                                                                        <FaPencil className="v-align-sub" /> Edit
-                                                                    </MenuItem>
-                                                                    {row && row.original && (typeof row.original.isDeleted === 'undefined' || row.original.isDeleted === 0) &&
-                                                                        <MenuItem eventKey="2" href="javascript:void(0)" onClick={() => this.confirmDelete(row.value)}>
-                                                                            <FaTrash className="v-align-sub" /> Delete
-                                                                        </MenuItem>
-                                                                    }
-                                                                    {row && row.original && typeof row.original.isDeleted !== 'undefined' && row.original.isDeleted === 1 &&
-                                                                        <MenuItem eventKey="3" href="javascript:void(0)" onClick={() => this.openRecoverModal(row.value)}>
-                                                                            <FaRotateLeft className="v-align-sub" /> Recover
-                                                                        </MenuItem>
-                                                                    }
-                                                                </DropdownButton>
-                                                            </ButtonToolbar>
+                                                            <Link to={`${adminRouteCodes.EQUIPMENTS_SAVE}/${row.value}`} className="dt-act-btn dt-act-btn-edit">
+                                                                <FaPencil />
+                                                            </Link>
+                                                            {row && row.original && (typeof row.original.isDeleted === 'undefined' || row.original.isDeleted === 0) &&
+                                                                <button className="dt-act-btn dt-act-btn-delete" onClick={() => this.confirmDelete(row.value)}>
+                                                                    <FaTrash />
+                                                                </button>
+                                                            }
+                                                            {row && row.original && typeof row.original.isDeleted !== 'undefined' && row.original.isDeleted === 1 &&
+                                                                <button className="dt-act-btn dt-act-btn-restore" onClick={() => this.openRecoverModal(row.value)}>
+                                                                    <FaRotateLeft />
+                                                                </button>
+                                                            }
                                                         </div>
                                                     );
-                                                },
+                                                }
                                             },
                                         ]}
                                         pages={dtPages}
